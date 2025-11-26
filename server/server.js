4578e3b71d5df4f580ab84ba82851d17802949d7
priya <bharathipriyama@gmail.com>
2025-11-26 16:45:09 +0530

    response tokens increased

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.post('/', async (req, res) => {
         },
       ],
       temperature: 0,
-      max_tokens: 700,
+      max_tokens: 3000,
       top_p: 1,
       frequency_penalty: 0.5,
       presence_penalty: 0,
